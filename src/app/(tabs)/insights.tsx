3f40fd5b8bf578d641886e7e6aad564fb55d16db
Le Duc Nam <arthit@Les-MacBook-Pro.local>
2026-03-29 21:26:58 +0700

    Bottom Tab layout

---
+++ src/app/(tabs)/insights.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const InsightScreen = () => {
+  return (
+    <View>
+      <Text>InsightScreen</Text>
+    </View>
+  );
+};
+
+export default InsightScreen;
